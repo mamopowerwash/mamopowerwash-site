378dbc34444a11b4dd52a8992d63be2d8ac44ceb
mamopowerwash <boucarfaye64@gmail.com>
2026-02-23 19:11:12 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,1 @@
-export const metadata = {
-  title: "Mamo Powerwash Solutions",
-};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <head />
-      <body style={{ margin: 0, fontFamily: "Arial" }}>
-        {children}
-      </body>
-    </html>
-  );
-}
