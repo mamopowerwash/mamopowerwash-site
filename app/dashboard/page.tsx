83f430ffceba055fb32c23629bf383ea4f435a95
mamopowerwash <boucarfaye64@gmail.com>
2026-02-26 08:07:47 -0500

    Delete app directory

--- app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>Dashboard</h1>
-      <p>Admin system will appear here.</p>
-    </main>
-  );
-}
--- app/login/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Login() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>Staff Login</h1>
-      <p>Login system will connect here.</p>
-    </main>
-  );
-}
